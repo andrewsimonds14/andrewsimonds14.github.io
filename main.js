3774ebb3b20ef675ca0e80b9ae1fd707d891f1b3
unknown <andrew.simonds@queensu.ca>
2019-02-02 15:18:08 -0500

    Started with Flask implementation

--- main.js
+++ main.js
@@ -4,23 +4,30 @@ $('#fin').on('click', function(e) {
     return $(radio).val();
   }).toArray();
 
+  var mood = 0; //Mood indexing: Happy - 3, Mellow - 2, Angry - 1, Sad - 0.
+
   if (choices.length >=4) {
     $('button[type=calculate]').css("display", "none");
     var count = 0;
     for (i = 0; i < choices.length; i++) {
       count += parseInt(choices[i]);
+
     }
     if (count > 18) {
       $('#happy').css("display", "block");
+      mood = 3;
     }
     else if (count > 12) {
       $('#mellow').css("display", "block");
+      mood = 2;
     }
     else if (count > 6) {
       $('#angry').css("display", "block");
+      mood = 1;
     }
     else {
       $('#sad').css("display", "block");
+      mood = 0;
     }
   }
 
---
+++ server/app.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/relaxnation/")
+def home():
+    return requests.get('http://andrewsimonds14.github.io').content
+
+@app.route("/relaxnation/quiz.html/")
+def quiz():
+    return requests.get('http://andrewsimonds14.github.io/quiz.html').content
